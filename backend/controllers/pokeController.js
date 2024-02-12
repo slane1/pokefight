37a3ca30b516f53be55556fb22000ccce9bb8b41
John Schulte <johannes-schulte@gmx.de>
2024-02-12 17:17:54 +0100

    router added + error handler

--- backend/controllers/pokeController.js
+++ backend/controllers/pokeController.js
@@ -1,17 +1,49 @@
 import { data } from "../data.js";
 
-export const getAllPokemon = (req, res) => {
-  res.json(data);
+export const getAllPokemon = (req, res, next) => {
+  try {
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getPokemonById = (req, res) => {
-  const { id } = req.params;
-  const result = data.find((c) => c.id === parseInt(id));
-  res.send(result);
+export const getPokemonById = (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = data.find((c) => c.id === parseInt(id));
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getPokemonDetail = (req, res) => {
-  const { id, detail } = req.params;
-  const result = data.find((c) => c.id === parseInt(id));
-  res.send(result[detail]);
+export const getPokemonDetail = (req, res, next) => {
+  try {
+    const { id, detail } = req.params;
+    const result = data.find((c) => c.id === parseInt(id));
+    res.json(result[detail]);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPokemonNameInEnglish = (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = data.find((c) => c.id === parseInt(id));
+    res.json(result["name"]["english"]);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPokemonBaseEntries = (req, res, next) => {
+  try {
+    const { id, info } = req.params;
+    const result = data.find((c) => c.id === parseInt(id));
+    res.json(result["base"][info]);
+  } catch (error) {
+    next(error);
+  }
 };
