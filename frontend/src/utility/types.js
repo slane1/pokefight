0378fdbb49bcaf7f0c78cad30dd350206d0c1fb9
Stefan Lange <stefanlange@maeve.de>
2024-02-20 15:43:42 +0100

    psychic added

--- frontend/src/utility/types.js
+++ frontend/src/utility/types.js
@@ -10,6 +10,7 @@ const typeMatchups = {
     "steel": {"normal": 1, "fighting": 1, "flying": 1, "poison": 1, "ground": 1, "rock": 2, "bug": 1, "ghost": 1, "steel": 0.5, "fire": 0.5, "water": 0.5, "grass": 1, "electric": 1, "psychic": 1, "ice": 2, "dragon": 1, "dark": 1, "fairy": 2},
     "fire": {"normal": 1, "fighting": 1, "flying": 1, "poison": 1, "ground": 1, "rock": 0.5, "bug": 2, "ghost": 1, "steel": 2, "fire": 0.5, "water": 0.5, "grass": 2, "electric": 1, "psychic": 1, "ice": 2, "dragon": 0.5, "dark": 1, "fairy": 1},
     "water": {"normal": 1, "fighting": 1, "flying": 1, "poison": 1, "ground": 2, "rock": 2, "bug": 1, "ghost": 1, "steel": 1, "fire": 2, "water": 0.5, "grass": 0.5, "electric": 1, "psychic": 1, "ice": 1, "dragon": 0.5, "dark": 1, "fairy": 1},
-    "grass": {"normal": 1, "fighting": 1, "flying": 0.5, "poison": 0.5, "ground": 2, "rock": 2, "bug": 0.5, "ghost": 1, "steel": 0.5, "fire": 0.5, "water": 2, "grass": 0.5, "electric": 1, "psychic": 1, "ice": 1, "dragon": 0.5, "dark": 1, "fairy": 1}
+    "grass": {"normal": 1, "fighting": 1, "flying": 0.5, "poison": 0.5, "ground": 2, "rock": 2, "bug": 0.5, "ghost": 1, "steel": 0.5, "fire": 0.5, "water": 2, "grass": 0.5, "electric": 1, "psychic": 1, "ice": 1, "dragon": 0.5, "dark": 1, "fairy": 1},
+    "psychic": {"normal": 0, "fighting": 1, "flying": 1, "poison": 1, "ground": 1, "rock": 1, "bug": 1, "ghost": 2, "steel": 1, "fire": 1, "water": 1, "grass": 1, "electric": 1, "psychic": 1, "ice": 1, "dragon": 1, "dark": 0.5, "fairy": 1}
 };
 export default typeMatchups;
